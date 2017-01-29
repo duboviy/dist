e516303f709dbd461c9a4826c1c0555f039fd675
Eugene Dubovoy <eugene.dubovoy@gmail.com>
2017-01-29 23:16:49 +0200

    Rename of src dir to proper release version name

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup, Extension
 
 
 if sys.version_info[0] == 3:    # "using Python 3"
-    ext = Extension("dist", sources=["src/py3/dist.c"])
+    ext = Extension("dist", sources=["dist/py3/dist.c"])
 elif sys.version_info[0] == 2:  # "using Python 2"
-    ext = Extension("dist", sources=["src/py2/dist.c"])
+    ext = Extension("dist", sources=["dist/py2/dist.c"])
 else:
     raise NotImplementedError("Not implemented for your python version")
 
