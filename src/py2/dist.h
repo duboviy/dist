c945960f66280401cf6beb7b232185e6846baae7
godbod <gkounkou@gmail.com>
2018-05-08 12:51:07 -0700

    This commit contains modifications for the dist tool. It removes hard coded constants and put them inside a dist.h header file.

---
+++ src/py2/dist.h
@@ -0,0 +1,14 @@
+/*
+ *	@(#)dist.h	 (Vancouver) 8/5/18
+ */
+
+#ifndef _DIST_H_
+#define _DIST_H_
+
+#define DEG_RAD           (M_PI / 180.0)
+#define RAD_DEG           (180  / M_PI) 
+#define STATUTE_PER_MILLE 1.1515
+#define KM_IN_MILLE       1.609344
+#define MINUTE_IN_DEGREE  60
+
+#endif /* !_DIST_H_ */
