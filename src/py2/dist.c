965cffe703da81c82d7b8877eb206aa6a7dea05b
Jacques <gkounkou@gmail.com>
2018-05-08 13:06:37 -0700

    Remove hard coded constants (#4)
    This commit contains modifications for the dist tool. It removes hard coded constants and put them inside a dist.h header file. Solve some indentations layout of the code for long expressions

--- src/py2/dist.c
+++ src/py2/dist.c
@@ -3,30 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
-#define d2r (M_PI / 180.0)
+#include "dist.h"
 
 
 // converts decimal degrees to radians
 double _deg2rad(double deg) {
-  return (deg * M_PI / 180.0);
+  return (deg * DEG_RAD);
 }
  
 // converts radians to decimal degrees
 double _rad2deg(double rad) {
-  return (rad * 180 / M_PI);
+  return (rad * RAD_DEG);
 }
 
 // use Haversine Formula to calculate distance
 double haversine(float lat1, float long1, float lat2, float long2)
 {
     float theta = long1 - long2;
-    float dist = sin(_deg2rad(lat1)) * sin(_deg2rad(lat2))
-	  		  + cos(_deg2rad(lat1)) * cos(_deg2rad(lat2)) * cos(_deg2rad(theta));
+    float dist = sin(_deg2rad(lat1)) * sin(_deg2rad(lat2)) 
+       + cos(_deg2rad(lat1)) * cos(_deg2rad(lat2)) * cos(_deg2rad(theta));
+
     dist = acos(dist);
     dist = _rad2deg(dist);
-    dist = dist * 60 * 1.1515 * 1.609344;
-	return dist;
+    dist = dist * MINUTE_IN_DEGREE * STATUTE_PER_MILLE * KM_IN_MILLE;
+    
+    return dist;
 }
 
 static PyObject* compute(PyObject* self, PyObject* args)
